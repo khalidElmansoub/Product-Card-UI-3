5da0b2434b9274db90c79aa7864de773b770d901
unknown <khalidelmansoub@example.com>
2023-09-19 14:42:14 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.size li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('.colors span').click(function(){
+
+        $(this).addClass('active').siblings().removeClass('active');
+        let image = $(this).attr('data-image');
+        $('.image img').attr('src',image);
+        let color = $(this).css('background');
+        $('.content .btn').css('background',color);
+    });
+});
